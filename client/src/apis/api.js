9e68c4e21a8fb69e2ace990e6f7a371baf3aa87b
pawankumar <pwnkumar0471@gmail.com>
2026-07-11 12:50:20 +0530

    Add production start script

--- client/src/apis/api.js
+++ client/src/apis/api.js
@@ -1,14 +1,12 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
 export const signupUser = (data) => API.post("/auth/register", data);
 export const loginUser = (data) => API.post("/auth/login", data);
-
-export const chatWithAI = (data) =>API.post("/ai/chat", data);
-
+export const chatWithAI = (data) => API.post("/ai/chat", data);
 
 export default API;
